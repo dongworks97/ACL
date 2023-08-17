f7bd874a5985a4b100cd61fcb97b4484032e25a0
dongworks97 <35809621+dongworks97@users.noreply.github.com>
2023-08-17 16:47:38 +0800

    Update diagnostic_metrics.R
    add MAD, MASE, SMAPE, AIC, BIC

--- R/diagnostic_metrics.R
+++ R/diagnostic_metrics.R
@@ -30,6 +30,10 @@ diagnostic_metrics <- function(data.CatL, model_result) {
   # Calculation MAE
   MAE <- mean(abs(errors))
 
+  # Calculation MASE
+  MAD <- mean(abs(errors-mean(observed_data)))
+  MASE <- MAE/MAD
+
   # Calculation RMSE
   RMSE <- sqrt(MSE)
 
@@ -45,14 +49,28 @@ diagnostic_metrics <- function(data.CatL, model_result) {
   # Calculate MAPE
   MAPE <- mean(abs((observed_data - estimated_data) / observed_data)) * 100
 
+  # Calculate SMAPE
+  SMAPE <- mean(abs(observed_data - estimated_data)/((abs(observed_data)+abs(estimated_data))/2)) * 100
+
   # Calculate Explained Variance Score
   exp_var_score <- 1 - var(observed_data - estimated_data) / var(observed_data)
 
   # Calculate Max Error
   max_error <- max(abs(observed_data - estimated_data))
 
+ #Calculate the parameters number 
+ par_num <- length(model_result[["obj"]][["par"]])
+
+ #Calculate the catch-at-length number 
+ cl_num <- length(observed_data)
+
+ #Calculate the AIC and BIC
+ AIC=2*par_num+2*(model_result[["opt"]][["objective"]])
+ BIC=par_num*log(cl_num)+2*(model_result[["opt"]][["objective"]])
+  
+
   # Create a data frame with the results
-  diagnostics <- data.frame(Metric = c("MSE", "MAE", "RMSE", "Rsquared", "MAPE", "Explained Variance Score", "Max Error"),
-                            Value = c(MSE, MAE, RMSE, Rsquared, MAPE, exp_var_score, max_error))
+  diagnostics <- data.frame(Metric = c("MSE", "MAE","MAD","MASE", "RMSE", "Rsquared", "MAPE", "SMAPE", "Explained Variance Score", "Max Error","AIC","BIC"),
+                            Value = c(MSE, MAE, MAD, MASE, RMSE, Rsquared, MAPE, SMAPE, exp_var_score, max_error, AIC, BIC))
   return(diagnostics)
 }
