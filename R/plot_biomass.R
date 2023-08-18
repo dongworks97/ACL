c3c057dd60ba71ac695eca992f64591152103ff2
dongworks97 <35809621+dongworks97@users.noreply.github.com>
2023-08-18 19:51:25 +0800

    Update plot_biomass.R

--- R/plot_biomass.R
+++ R/plot_biomass.R
@@ -39,84 +39,83 @@
 #' @export
 plot_biomass <- function(model_result, line_size = 1.2, line_color = "red", line_type = "solid", se = FALSE, se_color = "red", se_alpha = 0.2,type=c("B","BL"),facet_ncol = NULL, facet_scales = "free"){
   if(type=="B"){
-
-  # Extract the biomass data
-  biomass <- model_result[["report"]][["B"]]
-
-  # Make sure it's a data frame
-  if(!is.data.frame(biomass)){
-    biomass <- as.data.frame(biomass)
-  }
-
-  # Add Year to the biomass data
-  biomass$Year <-  model_result[["year"]]
-
-if(!se)
-  {
-  # Plot biomass over the years using ggplot2
-  p <- ggplot2::ggplot(biomass, aes(x = Year, y = biomass)) +
-    ggplot2::geom_line(size = line_size, color = line_color, linetype = line_type) +
-    ggplot2::labs(x = "Year", y = "Biomass", title = "Biomass Over Years") +
-    ggplot2::theme_minimal()
-}
-
-
-  else
-  {
-    # Filter rows that contain "B"
-    ss_bio <- model_result[["est_std"]][grep("^B", rownames(model_result[["est_std"]])),]
-
-    # Calculate confidence intervals
-    confidence_intervals_bio <- data.frame(
-      estimate = ss_bio[, "Estimate"],
-      lower = ss_bio[, "Estimate"] - 1.96 * ss_bio[, "Std. Error"],
-      upper = ss_bio[, "Estimate"] + 1.96 * ss_bio[, "Std. Error"]
-    )
-
-    confidence_intervals_bio$Year <-  model_result[["year"]]
-
-    # Plot recruitment over the years with confidence intervals using ggplot2
-    p <- ggplot2::ggplot(confidence_intervals_bio, aes(x = Year, y = estimate)) +
-      ggplot2::geom_line(size = line_size, color = line_color, linetype = line_type) +
-      ggplot2::geom_ribbon(aes(ymin = lower, ymax = upper),  fill = se_color,alpha = se_alpha) +
-      ggplot2::labs(y = "Biomass", x = "Year", title = "Biomass Over Years with Confidence Intervals") +
-      ggplot2::theme_minimal()
-  }
+    
+    # Extract the biomass data
+    biomass <- model_result[["report"]][["B"]]
+    
+    # Make sure it's a data frame
+    if(!is.data.frame(biomass)){
+      biomass <- as.data.frame(biomass)
+    }
+    
+    # Add Year to the biomass data
+    biomass$Year <-  model_result[["year"]]
+    
+    if(!se)
+    {
+      # Plot biomass over the years using ggplot2
+      p <- ggplot2::ggplot(biomass, aes(x = Year, y = biomass)) +
+        ggplot2::geom_line(size = line_size, color = line_color, linetype = line_type) +
+        ggplot2::labs(x = "Year", y = "Relative biomass", title = "Total Biomass Over Years") +
+        ggplot2::theme_minimal()+theme(plot.title = element_text(hjust = 0.5))
+    }
+    
+    
+    else
+    {
+      # Filter rows that contain "B"
+      ss_bio <- model_result[["est_std"]][grep("^B", rownames(model_result[["est_std"]])),]
+      
+      # Calculate confidence intervals
+      confidence_intervals_bio <- data.frame(
+        estimate = ss_bio[, "Estimate"],
+        lower = ss_bio[, "Estimate"] - 1.96 * ss_bio[, "Std. Error"],
+        upper = ss_bio[, "Estimate"] + 1.96 * ss_bio[, "Std. Error"]
+      )
+      
+      confidence_intervals_bio$Year <-  model_result[["year"]]
+      
+      # Plot recruitment over the years with confidence intervals using ggplot2
+      p <- ggplot2::ggplot(confidence_intervals_bio, aes(x = Year, y = estimate)) +
+        ggplot2::geom_line(size = line_size, color = line_color, linetype = line_type) +
+        ggplot2::geom_ribbon(aes(ymin = lower, ymax = upper),  fill = se_color,alpha = se_alpha) +
+        ggplot2::labs(y = "Relative biomass", x = "Year", title = "Total Biomass Over Years with Confidence Intervals") +
+        ggplot2::theme_minimal()+theme(plot.title = element_text(hjust = 0.5))
+    }
   }
   if (type=="BL"){
-
+    
     # Extract the BL data
     BL <- model_result[["report"]][["BL"]]
-
+    
     # Make sure it's a matrix
     if(!is.matrix(BL)){
       BL <- as.matrix(BL)
     }
-
+    
     # Create Year variable from column names of BL (assuming columns are years)
     Year <- model_result[["year"]]
-
+    
     # Create LengthGroup variable from row names of BL
     LengthGroup <- paste0("Length bin ", seq_len(nrow(BL)))
     LengthGroup <- factor(paste("Length bin ", seq_len(nrow(BL))), levels=paste("Length bin ", seq_len(nrow(BL))))
-
+    
     # Convert matrix to data frame in long format
     BL_long <- reshape2::melt(BL)
     colnames(BL_long) <- c("LengthGroup", "Year", "Count")
     BL_long$Year <- Year[match(BL_long$Year, 1:length(Year))]
     BL_long$LengthGroup <- LengthGroup[as.numeric(BL_long$LengthGroup)]
-
-
-
-
+    
+    
+    
+    
     # Plot BL over the years using ggplot2
     p <- ggplot2::ggplot(BL_long, aes(x = Year, y = Count)) +
       ggplot2::geom_line( size = line_size, color = line_color, linetype = line_type) +
       ggplot2::facet_wrap(~LengthGroup, ncol = facet_ncol, scales = facet_scales) +
-      ggplot2::labs(x = "Year", y = "Relative abundance", title = "Biomass at length group Over Years") +
-      ggplot2::theme_minimal()
-
+      ggplot2::labs(x = "Year", y = "Relative biomass", title = "Total Biomass at length group Over Years") +
+      ggplot2::theme_minimal()+theme(plot.title = element_text(hjust = 0.5))
+    
   }
   return(p)
 }
-
