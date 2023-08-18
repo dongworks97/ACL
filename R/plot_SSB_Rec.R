844a97294c75392bcd8f52b6afe3b0f0e4e3bf7d
dongworks97 <35809621+dongworks97@users.noreply.github.com>
2023-08-18 20:29:33 +0800

    Update plot_SSB_Rec.R

--- R/plot_SSB_Rec.R
+++ R/plot_SSB_Rec.R
@@ -23,21 +23,36 @@
 #' print(p)
 #' }
 #' @export
-plot_SSB_Rec <- function(model_result,point_size=2,point_color="black",point_shape=16) {
-
-
+plot_SSB_Rec <- function(model_result,point_size=2,point_color="black",point_shape=16,relationship=FALSE) {
+  
+  
   # Extract the necessary data
   SSB_data <- model_result[["report"]][["SSB"]]
   Rec_data <- model_result[["report"]][["Rec"]]
-
+  
   # Create a data frame for ggplot
   plot_data <- data.frame(SSB = SSB_data, Rec = Rec_data)
-
+  fit_ssbrec=lm(Rec~SSB,data=plot_data)
+  summary(fit_ssbrec)
   # Create the plot
+  library(ggpmisc)
+  if (!relationship){
+    plot <-  ggplot2::ggplot(plot_data, aes(x = SSB, y = Rec)) +
+    ggplot2::geom_point(size= point_size, color= point_color,shape=point_shape) +
+    ggplot2:: labs(x = "Spawning stock biomass", y = "Recruitment",title = "Stock-recruitment relationship plot ")+
+      theme_minimal()+theme(plot.title = element_text(hjust = 0.5))
+    
+    
+  }else{
+  
+  
   plot <-  ggplot2::ggplot(plot_data, aes(x = SSB, y = Rec)) +
     ggplot2::geom_point(size= point_size, color= point_color,shape=point_shape) +
-    ggplot2:: labs(x = "SSB", y = "Rec")+
-    ggplot2::theme_minimal()
-
+    ggplot2:: labs(x = "Spawning stock biomass", y = "Recruitment",title = "Stock-recruitment relationship plot ")+
+    ggplot2::theme_minimal()+geom_smooth(method = 'lm', formula = y~x, se = TRUE, show.legend = FALSE) + 
+    stat_poly_eq(aes(label = paste(..eq.label.., ..rr.label.., stat(p.value.label), sep = '~`,`~')),
+                 formula = y~x, parse = TRUE, label.x.npc = 'left', label.y.npc = 'top', 
+                 size = 5 ,color= "blue")+theme(plot.title = element_text(hjust = 0.5))
+  }
   return(plot)
 }
